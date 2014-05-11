9bfe6bf6a56ec04fff0c6db8232c85b9accbdf3f
nicolewhite <nicole.m.white@outlook.com>
2014-05-11 13:33:36 -0500

    Tidy up addLabel()

--- R/addLabel.R
+++ R/addLabel.R
@@ -4,20 +4,17 @@ addLabel.default = function(x) {
   stop("Invalid object. Must supply node object.")
 }
 
-addLabel.node = function(node, label = character()) {
-  stopifnot(is.character(label))
+addLabel.node = function(node, ...) {
+  labels = c(...)
+  stopifnot(is.character(labels), length(labels) > 0)
   
-  if(length(label) == 0)
-    return()
-  
-    if(length(label) == 1) {
-    fields = paste0(' "', label, '" ')
+    if(length(labels) == 1) {
+    fields = paste0(' "', labels, '" ')
   } else {
-    fields = toJSON(label)
+    fields = toJSON(labels)
   }
 
-  headers = list('Accept' = 'application/json', 'Content-Type' = 'application/json')
+  headers = list('Accept' = 'application/json', 'Content-Type' = 'application/json') 
   httpPOST(node$labels, httpheader = headers, postfields = fields)
-  
   return(invisible(NULL))
 }
