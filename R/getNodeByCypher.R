f0d58306881c8d3c07c011bc64fe856be2a3f2e3
Nicole White <nicole.m.white@outlook.com>
2014-05-14 17:42:12 -0500

    Add parameter functionality to getNodeByCypher and getRelByCypher.

--- R/getNodeByCypher.R
+++ R/getNodeByCypher.R
@@ -1,15 +1,21 @@
-getNodeByCypher = function(graph, query) UseMethod("getNodeByCypher")
+getNodeByCypher = function(graph, query, ...) UseMethod("getNodeByCypher")
 
 getNodeByCypher.default = function(x, ...) {
   stop("Invalid object. Must supply graph object.")
 }
 
-getNodeByCypher.graph = function(graph, query) {
+getNodeByCypher.graph = function(graph, query, ...) {
   stopifnot(is.character(query),
             length(query) == 1)
 
   headers = list('Accept' = 'application/json', 'Content-Type' = 'application/json')
-  fields = toJSON(list(query = query))
+  params = list(...)  
+  fields = list(query = query)
+
+  if(length(params) > 0)
+    fields = c(fields, params = list(params))
+
+  fields = toJSON(fields)
   response = fromJSON(httpPOST(attr(graph, "cypher"), httpheader = headers, postfields = fields))
   result = response$data[[1]][[1]]
   class(result) = c("entity", "node")
--- R/getRelByCypher.R
+++ R/getRelByCypher.R
@@ -1,15 +1,21 @@
-getRelByCypher = function(graph, query) UseMethod("getRelByCypher")
+getRelByCypher = function(graph, query, ...) UseMethod("getRelByCypher")
 
 getRelByCypher.default = function(x, ...) {
   stop("Invalid object. Must supply graph object.")
 }
 
-getRelByCypher.graph = function(graph, query) {
+getRelByCypher.graph = function(graph, query, ...) {
   stopifnot(is.character(query),
             length(query) == 1)
-  
+
   headers = list('Accept' = 'application/json', 'Content-Type' = 'application/json')
-  fields = toJSON(list(query = query))
+  params = list(...)  
+  fields = list(query = query)
+
+  if(length(params) > 0)
+    fields = c(fields, params = list(params))
+
+  fields = toJSON(fields)
   response = fromJSON(httpPOST(attr(graph, "cypher"), httpheader = headers, postfields = fields))
   result = response$data[[1]][[1]]
   class(result) = c("entity", "relationship")
