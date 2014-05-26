dc77ea4de484b5eae83638737cc66d3481d268ec
nicolewhite <nicole.m.white@outlook.com>
2014-05-26 12:05:31 -0500

    Add getOrCreateNode

---
+++ R/getOrCreateNode.R
@@ -0,0 +1,21 @@
+getOrCreateNode = function(graph, label, ...) UseMethod("getOrCreateNode")
+
+getOrCreateNode.default = function(x, ...) {
+  stop("Invalid object. Must supply graph object.")
+}
+
+getOrCreateNode.graph = function(graph, label, ...) {
+  stopifnot(is.character(label))
+  props = c(...)
+  if(!(names(props)[1] %in% getConstraint(graph, label)$property_keys)) {
+    stop("The first key = value pair listed in ... must be uniquely constrained for the given node label.")
+  }
+            
+  node = try(createNode(graph, label, ...), TRUE)
+  
+  if("try-error" %in% class(node)) {
+    node = getUniqueNode(graph, label, props[1])
+  }
+  
+  return(node)
+}
