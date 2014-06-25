5342ae9f12909997c710cc6c357971aaa7fcfbca
nicolewhite <nicole.m.white@outlook.com>
2014-06-24 22:37:59 -0500

    Add username/password functionality to startGraph.

--- R/startGraph.R
+++ R/startGraph.R
@@ -1,24 +1,35 @@
-startGraph = function(url) UseMethod("startGraph")
+startGraph = function(url, username = character(), password = character()) UseMethod("startGraph")
 
-startGraph.default = function(url) {
+startGraph.default = function(url, username = character(), password = character()) {
   stopifnot(is.character(url), 
-            length(url) == 1)
-  response = http_request(url,
-                          "GET",
-                          "OK")
-  result = fromJSON(response)
+            length(url) == 1,
+            is.character(username),
+            is.character(password))
   
+  if(length(username) == 1 && length(password) == 1) {
+    userpwd = paste0(username, ":", password)
+    url = gsub("http://", paste0("http://", userpwd, "@"), url)
+    response = http_request(url,"GET","OK")
+  } else {
+    response = http_request(url,"GET","OK")
+  }
+  
+  if(class(response) == "try-error") {
+    stop("Connection failed. Check your URL and don't forget the trailing forward-slash.")
+  }
+  
+  result = fromJSON(response)
   graph = list()
   graph$version = result$neo4j_version
-  attr(graph, "node") = result$node
-  attr(graph, "node_index") = result$node_index
-  attr(graph, "relationship_index") = result$relationship_index
-  attr(graph, "relationship_types") = result$relationship_types
-  attr(graph, "batch") = result$batch
-  attr(graph, "cypher") = result$cypher
-  attr(graph, "indexes") = result$indexes
-  attr(graph, "constraints") = result$constraints
-  attr(graph, "node_labels") = result$node_labels
+  attr(graph, "node") = paste0(url, "node")
+  attr(graph, "node_index") = paste0(url, "index/node")
+  attr(graph, "relationship_index") = paste0(url, "index/relationship")
+  attr(graph, "relationship_types") = paste0(url, "relationship/types")
+  attr(graph, "batch") = paste0(url, "batch")
+  attr(graph, "cypher") = paste0(url, "cypher")
+  attr(graph, "indexes") = paste0(url, "schema/index")
+  attr(graph, "constraints") = paste0(url, "schema/constraint")
+  attr(graph, "node_labels") = paste0(url, "labels")
   
   # Remove trailing forward slash.
   url = substr(url, 1, nchar(url) - 1)
