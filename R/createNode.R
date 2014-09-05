816005b1d3066bf2d9f6d6c8f77c5ec1ef76a7db
nicolewhite <nicole.white@neotechnology.com>
2014-09-04 17:28:26 -0700

    Add dots before parameters.

--- R/createNode.R
+++ R/createNode.R
@@ -1,17 +1,16 @@
-createNode = function(graph, label = character(), ...) UseMethod("createNode")
+createNode = function(graph, .label = character(), ...) UseMethod("createNode")
 
 createNode.default = function(x, ...) {
   stop("Invalid object. Must supply graph object.")
 }
 
-createNode.graph = function(graph, label = character(), ...) {
-  stopifnot(is.character(label))
+createNode.graph = function(graph, .label = character(), ...) {
+  stopifnot(is.character(.label))
   
   header = setHeaders()
   fields = NULL
   
   params = list(...)
-  
   if(length(params) > 0)
     fields = toJSON(params)
   
@@ -26,11 +25,11 @@ createNode.graph = function(graph, label = character(), ...) {
   class(result) = c("node", "entity")
   node = configure_result(result, attr(graph, "username"), attr(graph, "password"))
 
-  if(length(label) > 0) {
-    if(length(grep(" ", label)) > 0) {
+  if(length(.label) > 0) {
+    if(length(grep(" ", .label)) > 0) {
       stop("Cannot have spaces in labels. Use CamelCase instead.")
     }
-    test = try(addLabel(node, label), TRUE)
+    test = try(addLabel(node, .label), TRUE)
     if("try-error" %in% class(test)) {
       delete(node)
       stop(test)
--- R/createRel.R
+++ R/createRel.R
@@ -1,28 +1,31 @@
-createRel = function(fromNode, type, toNode, ...) UseMethod("createRel")
+createRel = function(.fromNode, .relType, .toNode, ...) {
+  UseMethod("createRel")
+}
 
-createRel.default = function(x, ...) {
+createRel.default = function(a, ...) {
   stop("Invalid object. Must supply node object.")
 }
 
-createRel.node = function(fromNode, type, toNode, ...) {
-  stopifnot(is.character(type), 
-            "node" %in% class(toNode))
+createRel.node = function(.fromNode, .relType, .toNode, ...) {
+  stopifnot(is.character(.relType), 
+            "node" %in% class(.toNode))
   
-  if(length(grep(" ", type)) > 0) {
+  if(length(grep(" ", .relType)) > 0) {
     stop("Cannot have spaces in relationship types. Use UNDER_SCORES instead.")
   }
   
   header = setHeaders()
   
-  fields = list(to = attr(toNode, "self"), type = type)
+  fields = list(to = attr(.toNode, "self"), type = .relType)
   
   params = list(...)
+  
   # If user supplied properties, append them to request.
   if(length(params) > 0)
     fields = c(fields, data = list(params))
   
   fields = toJSON(fields)
-  url = attr(fromNode, "create_relationship")
+  url = attr(.fromNode, "create_relationship")
   response = http_request(url,
                           "POST",
                           "Created",
