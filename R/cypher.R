a429d77bcde35482eeb9b1f7afefdc63ed248c40
nicolewhite <nicole.white@neotechnology.com>
2014-10-17 18:16:31 -0700

    Add functionality for returning variable-length collections as columns in a data frame.

--- R/cypher.R
+++ R/cypher.R
@@ -29,19 +29,33 @@ cypher.graph = function(graph, query, ...) {
   
   result = fromJSON(response)
   data = result$data
+  if(length(data) == 0) {
+    return(invisible(NULL))
+  }
+  ### Stolen from: http://stackoverflow.com/questions/22870198/is-there-a-more-efficient-way-to-replace-null-with-na-in-a-list
+  nullToNA <- function(x) {
+    x[sapply(x, is.null)] = NA
+    return(x)
+  }
+  ###
+  data = do.call(rbind, data)
+  data = nullToNA(data)
+  options(stringsAsFactors = FALSE)
+  df = data.frame(data)
   
-  replaceNULL = function(row) {
-    new = lapply(row, function(r) ifelse(is.null(r), NA, r))
-    return(new)
+  # Get the maximum length of nested lists.
+  checkNested <- function(col) {
+    max(unlist(sapply(col, function(x) {sapply(x, length)})))
   }
   
-  data = lapply(data, replaceNULL)
-  options(stringsAsFactors = FALSE)
-  df = do.call(rbind.data.frame, data)
-  
-  if(is.empty(df)) {
-    return(invisible(NULL))
-  } 
+  # Unlist columns that aren't variable-length.
+  if(all(sapply(df, class) == "list")) {
+    for(i in 1:ncol(df)) {
+      if(checkNested(df[i]) == 1) {
+        df[i] = unlist(df[i])
+      } 
+    }
+  }
   
   names(df) = result$columns
   row.names(df) = NULL
