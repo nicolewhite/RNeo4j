4a439959c1937c104ab827cd074efae92524f6c4
nicolewhite <nicole.white@neotechnology.com>
2014-10-28 23:11:43 -0700

    Add getSinglePath and getPaths.

---
+++ R/getPaths.R
@@ -0,0 +1,44 @@
+getPaths = function(graph, query, ...) UseMethod("getPaths")
+
+getPaths.default = function(x, ...) {
+  stop("Invalid object. Must supply graph object.")
+}
+
+getPaths.graph = function(graph, query, ...) {
+  stopifnot(is.character(query),
+            length(query) == 1)
+  
+  header = setHeaders()
+  params = list(...)  
+  fields = list(query = query)
+  
+  if(length(params) > 0) {
+    fields = c(fields, params = list(params))
+    max_digits = find_max_dig(params)
+  }
+  
+  fields = toJSON(fields, digits = max_digits)
+  url = attr(graph, "cypher")
+  response = http_request(url,
+                          "POST",
+                          "OK",
+                          fields,
+                          header)
+  
+  result = fromJSON(response)
+  result = result$data
+  
+  set_class = function(i) {
+    current = result[[i]][[1]]
+    is.path = try(current$self, silent = T)
+    if(!is.null(is.path) | class(is.path) == "try-error") {
+      stop("At least one entity returned is not a path. Check that your query is returning paths.")
+    }
+    class(current) = "path"
+    return(current)
+  }
+  
+  result = lapply(1:length(result), set_class)
+  paths = lapply(result, function(r) configure_result(r, attr(graph, "username"), attr(graph, "password")))
+  return(paths)
+}
---
+++ R/getSinglePath.R
@@ -0,0 +1,43 @@
+getSinglePath = function(graph, query, ...) UseMethod("getSinglePath")
+
+getSinglePath.default = function(x, ...) {
+  stop("Invalid object. Must supply graph object.")
+}
+
+getSinglePath.graph = function(graph, query, ...) {
+  stopifnot(is.character(query),
+            length(query) == 1)
+  
+  header = setHeaders()
+  params = list(...)  
+  fields = list(query = query)
+  
+  if(length(params) > 0) {
+    fields = c(fields, params = list(params))
+    max_digits = find_max_dig(params)
+  }
+  
+  fields = toJSON(fields, digits = max_digits)
+  url = attr(graph, "cypher")
+  response = http_request(url,
+                          "POST",
+                          "OK",
+                          fields,
+                          header)
+  
+  result = fromJSON(response)
+  
+  if(length(result$data) == 0) {
+    message("Path not found.")
+    return(invisible(NULL))
+  }
+  
+  result = result$data[[1]][[1]]
+  is.path = try(result$self, silent = T)
+  if(!is.null(is.path) | class(is.path) == "try-error") {
+    stop("The entity returned is not a path. Check that your query is returning a path.")
+  }
+  class(result) = "path"
+  path = configure_result(result, attr(graph, "username"), attr(graph, "password"))
+  return(path)
+}
