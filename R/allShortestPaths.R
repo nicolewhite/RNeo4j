fb72e51216047569b0511fa2f10a95589efc2abe
nicolewhite <nicole.white@neotechnology.com>
2014-10-28 23:13:05 -0700

    Add shortest path functions.

---
+++ R/allShortestPaths.R
@@ -0,0 +1,52 @@
+allShortestPaths = function(fromNode, relType, toNode, direction = "out", max_depth = 1) UseMethod("allShortestPaths")
+
+allShortestPaths.default = function(x, ...) {
+  stop("Invalid object. Must supply node object.")
+}
+
+allShortestPaths.node = function(fromNode, relType, toNode, direction = "out", max_depth = 1) {
+  stopifnot(is.character(relType), 
+            "node" %in% class(toNode),
+            direction %in% c("in", "out"),
+            is.numeric(max_depth))
+  
+  header = setHeaders()
+  
+  url = paste(attr(fromNode, "self"), "paths", sep = "/")
+  to = attr(toNode, "self")
+  fields = list(to = to,
+                max_depth = max_depth,
+                relationships = list(type = relType,
+                                     direction = direction),
+                algorithm = "shortestPath")
+  
+  fields = toJSON(fields)
+  
+  response = try(http_request(url,
+                              "POST",
+                              "OK",
+                              postfields = fields,
+                              httpheader = header),
+                 silent = T)
+  
+  if(class(response) == "try-error") {
+    message("No paths found.")
+    return(invisible(NULL))
+  }
+  
+  result = fromJSON(response)
+  
+  if(length(result) == 0) {
+    message("No paths found.")
+    return(invisible(NULL))
+  }
+  
+  set_class = function(r) {
+    class(r) = "path"
+    return(r)
+  }
+  
+  result = lapply(result, set_class)
+  paths = lapply(result, function(r) configure_result(r, attr(graph, "username"), attr(graph, "password")))
+  return(paths)
+}
---
+++ R/shortestPath.R
@@ -0,0 +1,42 @@
+shortestPath = function(fromNode, relType, toNode, direction = "out", max_depth = 1) UseMethod("shortestPath")
+
+shortestPath.default = function(x, ...) {
+  stop("Invalid object. Must supply node object.")
+}
+
+shortestPath.node = function(fromNode, relType, toNode, direction = "out", max_depth = 1) {
+  stopifnot(is.character(relType), 
+            "node" %in% class(toNode),
+            direction %in% c("in", "out"),
+            is.numeric(max_depth))
+  
+  header = setHeaders()
+  
+  url = paste(attr(fromNode, "self"), "path", sep = "/")
+  to = attr(toNode, "self")
+  fields = list(to = to,
+                max_depth = max_depth,
+                relationships = list(type = relType,
+                                     direction = direction),
+                algorithm = "shortestPath")
+  
+  fields = toJSON(fields)
+  
+  response = try(http_request(url,
+                          "POST",
+                          "OK",
+                          postfields = fields,
+                          httpheader = header),
+                 silent = T)
+  
+  if(class(response) == "try-error") {
+    message("No path found.")
+    return(invisible(NULL))
+  }
+  
+  result = fromJSON(response)
+  
+  class(result) = "path"
+  path = configure_result(result, attr(fromNode, "username"), attr(fromNode, "password"))
+  return(path)
+}
