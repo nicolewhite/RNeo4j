fb47330126a0012fa415d7d319f64e4e70d9de85
nicolewhite <nicole.white@neotechnology.com>
2014-11-02 16:58:45 -0800

    Add more sample datasets.

--- R/importSample.R
+++ R/importSample.R
@@ -9,12 +9,36 @@ importSample.graph = function(graph, data) {
   clear(graph)
   
   if(data == "movies") {
+    addConstraint(graph, "Person", "name")
+    addConstraint(graph, "Movie", "title")
     fpath = system.file("extdata", "movies.txt", package = "RNeo4j")
-    
-  } else {
-    stop("As of now, only the movie dataset is available. Set data = 'movies'.")
+  } else if(data == "dfw") {
+    addConstraint(graph, "Terminal", "name")
+    addConstraint(graph, "Place", "name")
+    addConstraint(graph, "Category", "name")
+    fpath = system.file("extdata", "dfw.txt", package = "RNeo4j")
+  } else if(data == "caltrain") {
+    addConstraint(graph, "Stop", "name")
+    addConstraint(graph, "Zone", "id")
+    addConstraint(graph, "Train", "id")
+    fpath = system.file("extdata", "caltrain.txt", package = "RNeo4j")
+  } else if(data == "fleets") {
+    addConstraint(graph, "Model", "name")
+    addConstraint(graph, "Series", "name")
+    addConstraint(graph, "Airline", "name")
+    addConstraint(graph, "Country", "name")
+    fpath = system.file("extdata", "fleets.txt", package = "RNeo4j")
+  } else if(data == "tweets") {
+    addConstraint(graph, "Hashtag", "name")
+    addConstraint(graph, "Link", "url")
+    addConstraint(graph, "Source", "name")
+    addConstraint(graph, "Tweet", "id")
+    addConstraint(graph, "User", "screen_name")
+    fpath = system.file("extdata", "tweets.txt", package = "RNeo4j")
+  }
+    else {
+    stop("Invalid dataset.")
   }
   query = readChar(fpath, file.info(fpath)$size)
   suppressMessages(cypher(graph, query))
 }
-
