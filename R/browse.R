11fe3744e05eb2f2c2b1668363da9b3334ceabdd
Kenny Darrell <darrelkj@gmail.com>
2014-11-23 12:12:50 -0500

    Add swith for viewer pane

--- R/browse.R
+++ R/browse.R
@@ -4,8 +4,8 @@ browse.default = function(x, ...) {
   stop("Invalid object. Must supply graph object.")
 }
 
-browse.graph = function(graph) {
-  if (Sys.getenv("RSTUDIO") == "1") {
+browse.graph = function(graph, viewer = FALSE) {
+  if (Sys.getenv("RSTUDIO") == "1" & viewer) {
     rstudio::viewer(sub("db/data", "browser", attr(graph, "root")))
   } else {
     browseURL(sub("db/data", "browser", attr(graph, "root")))
