115744f5411b685ca9b25d36293f6398649bfb81
nicolewhite <nicole.white@neotechnology.com>
2015-05-14 12:34:11 +0200

    Add test for transactional endpoint.

---
+++ tests/testthat/test-transactions.R
@@ -0,0 +1,46 @@
+library(RNeo4j)
+context("Transactions")
+
+username = Sys.getenv("NEO4J_USERNAME")
+password = Sys.getenv("NEO4J_PASSWORD")
+
+neo4j = startGraph("http://localhost:7474/db/data/", username, password)
+clear(neo4j, input=F)
+
+test_that("transactions work", {
+  actual = data.frame(Origin = c("SFO", "AUS", "MCI"),
+                      FlightNum = c(1, 2, 3),
+                      Destination = c("PDX", "MCI", "LGA"))
+  
+  query = "
+  MERGE (origin:Airport {name:{origin_name}})
+  MERGE (destination:Airport {name:{dest_name}})
+  CREATE (origin)<-[:ORIGIN]-(:Flight {number:{flight_num}})-[:DESTINATION]->(destination)
+  "
+
+  t = newTransaction(neo4j)
+  
+  for (i in 1:nrow(actual)) {
+    origin_name = actual[i, ]$Origin
+    dest_name = actual[i, ]$Dest
+    flight_num = actual[i, ]$FlightNum
+    
+    appendCypher(t, 
+                 query, 
+                 origin_name = origin_name, 
+                 dest_name = dest_name, 
+                 flight_num = flight_num)
+  }
+  
+  commit(t)
+  
+  q = "
+  MATCH (o:Airport)<-[:ORIGIN]-(f:Flight)-[:DESTINATION]->(d:Airport)
+  RETURN o.name AS Origin, f.number AS FlightNum, d.name AS Destination
+  ORDER BY FlightNum
+  "
+  
+  expected = cypher(neo4j, q)
+  
+  expect_identical(actual, expected)
+})
