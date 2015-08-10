9546ac73a4e6fb0411e2a1ea844724b506d9b597
Nicole White <nicole.m.white@outlook.com>
2015-08-10 12:23:16 -0700

    Suppress warnings when printing
    Warnings were being printed in the case that the entity has no
    properties.

--- R/print.R
+++ R/print.R
@@ -11,21 +11,21 @@ summary.graph = function(graph) {
 
 print.node = function(node) {
   cat("< Node Object > \n")
-  if(any(!is.na(names(node)))) {
+  if(suppressWarnings(any(!is.na(names(node))))) {
     invisible(lapply(names(node), function(x) {print(node[x])}))
   }
 }
 
 print.relationship = function(rel) {
   cat("< Relationship Object > \n")
-  if(any(!is.na(names(rel)))) {
+  if(suppressWarnings(any(!is.na(names(rel))))) {
     invisible(lapply(names(rel), function(x) {print(rel[x])}))
   }
 }
 
 print.path = function(path) {
   cat("< Path Object > \n")
-  if(any(!is.na(names(path)))) {
+  if(suppressWarnings(any(!is.na(names(path))))) {
     invisible(lapply(names(path), function(x) {print(path[x])}))
   }
 }
