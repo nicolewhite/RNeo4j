28c4479c2dc87ca0a1c435315640725f0d4dc9f4
Nicole White <nicole.m.white@outlook.com>
2015-09-09 09:34:28 -0700

    Add help page for ?RNeo4j

---
+++ R/RNeo4j.R
@@ -0,0 +1,8 @@
+#' @title Neo4j Driver for R
+#' @author Nicole White
+#' @name RNeo4j
+#' @description Neo4j's R driver. RNeo4j allows you to read and write data from / to Neo4j directly from your R environment. For bug reports, please
+#' \href{https://github.com/nicolewhite/RNeo4j/issues}{create a new GitHub issue}. 
+#' For support, please \href{http://stackoverflow.com/questions/tagged/r-neo4j}{create a question on StackOverflow}.
+#'
+NULL
