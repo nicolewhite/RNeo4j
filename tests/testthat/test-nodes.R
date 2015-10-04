7e9e0b8a4a49f136b1e17c6fe7b0b06369b52de7
Nicole White <nicole.m.white@outlook.com>
2015-10-04 14:40:52 -0700

    Clear the db before each test in test-nodes

--- tests/testthat/test-nodes.R
+++ tests/testthat/test-nodes.R
@@ -8,24 +8,19 @@ options(digits=20)
 neo4j = startGraph("http://localhost:7474/db/data/", "neo4j", "password")
 clear(neo4j, input=F)
 
-mugshots = createNode(neo4j, "Bar", name="Mugshots", location="México")
-nastys = createNode(neo4j, "Bar", name="Nasty's")
-
-test_that("createNode works", {
-  expect_identical(class(mugshots), c("entity", "node"))
-  expect_identical(class(nastys), c("entity", "node"))
-})
-
 test_that("createNode works without any properties", {
+  clear(neo4j, input=F)
   n = createNode(neo4j, "Person")
   expect_equal(length(n), 0)
 })
 
 test_that("createNode works without any properties or labels", {
+  clear(neo4j, input=F)
   createNode(neo4j)
 })
 
 test_that("createNode doesn't round numeric parameters", {
+  clear(neo4j, input=F)
   AGE = 123456789
   n = createNode(neo4j, "Person", age=AGE)
   n = getSingleNode(neo4j, "MATCH n WHERE n.age = {age} RETURN n", age=AGE)
@@ -33,6 +28,11 @@ test_that("createNode doesn't round numeric parameters", {
 })
 
 test_that("getNodes works", {
+  clear(neo4j, input=F)
+  
+  mugshots = createNode(neo4j, "Bar", name="Mugshots", location="México")
+  nastys = createNode(neo4j, "Bar", name="Nasty's")
+  
   nodes = getNodes(neo4j, "MATCH n RETURN n")
   
   names = sapply(nodes, '[[', 'name')
@@ -41,28 +41,45 @@ test_that("getNodes works", {
 })
 
 test_that("getSingleNode works", {
+  clear(neo4j, input=F)
+  mugshots = createNode(neo4j, "Bar", name="Mugshots", location="México")
   node = getSingleNode(neo4j, "MATCH (b:Bar {name:'Mugshots'}) RETURN b")
   expect_equal(node$location, "México")
 })
 
 test_that("getUniqueNode works", {
+  clear(neo4j, input=F)
+  mugshots = createNode(neo4j, "Bar", name="Mugshots", location="México")
+  
   addConstraint(neo4j, "Bar", "name")
   node = getUniqueNode(neo4j, "Bar", name="Mugshots")
   expect_identical(class(node), c("entity", "node"))
 })
 
 test_that("getOrCreateNode works", {
+  clear(neo4j, input=F)
+  addConstraint(neo4j, "Bar", "name")
+  
+  mugshots = createNode(neo4j, "Bar", name="Mugshots", location="México")
+  
   node = getOrCreateNode(neo4j, "Bar", name="Mugshots")
   expect_identical(class(node), c("entity", "node"))
 })
 
 test_that("getOrCreateNode works with last of parameters", {
+  clear(neo4j, input=F)
+  addConstraint(neo4j, "Bar", "name")
+  
+  mugshots = createNode(neo4j, "Bar", name="Mugshots", location="México")
+  
   node = getOrCreateNode(neo4j, "Bar", list(name="Mugshots", other="Other"))
   expect_identical(class(node), c("entity", "node"))
   expect_null(node$other)
 })
 
 test_that("getLabeledNodes works", {
+  clear(neo4j, input=F)
+  
   node1 = createNode(neo4j, "Something")
   node2 = createNode(neo4j, "Something", prop=1)
   
@@ -80,13 +97,18 @@ test_that("getLabeledNodes works", {
 })
 
 test_that("delete works", {
+  clear(neo4j, input=F)
+  
   n = createNode(neo4j, "Thing", name="Nicole")
   delete(n)
+  
   n = getSingleNode(neo4j, "MATCH (n:Thing) WHERE n.name = 'Nicole' RETURN n")
   expect_null(n)
 })
 
 test_that("createNode can set properties passed as a list", {
+  clear(neo4j, input=F)
+  
   n = createNode(neo4j, "Thing", list(name="Nicole", age=24))
   
   expect_equal(n$name, "Nicole")
